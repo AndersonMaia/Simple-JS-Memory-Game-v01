099abb0ecd93f9350903784846287e8f9d919702
Anderson maia <65310650+AndersonMaia@users.noreply.github.com>
2021-01-31 11:07:43 -0300

    Add files via upload

--- js-memory-game-v1/app.js
+++ js-memory-game-v1/app.js
@@ -77,14 +77,14 @@ function checkForMatch(){
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
     if(cardsChosen[0] === cardsChosen[1]){
-        alert('You found a match');
+        resultDisplay.textContent = 'You found a match';
         cards[optionOneId].setAttribute('src', 'images/black.png');
         cards[optionTwoId].setAttribute('src', 'images/black.png');
         cardsWon.push(cardsChosen);
     }else{
             cards[optionOneId].setAttribute('src', 'images/blank.png');
             cards[optionTwoId].setAttribute('src', 'images/blank.png');
-            alert('Sorry, try again');
+            resultDisplay.textContent = 'Sorry, try again';
     }
     cardsChosen = [];
     cardsChosenId = [];
